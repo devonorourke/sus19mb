485bb2dcbe2a0ea55424933c01a15ad6e5cf7b3c
Niel <niel@hssph0000763.hs.wvu-ad.wvu.edu>
2019-05-17 16:00:03 -0400

    Add gplots

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,4 @@
-pkgs = c("reshape2", "tidyverse", "plotly","upsetr","ggpubr","knitr", "CaTools", "bitops", "rprojroot", "rmarkdown","scales")
+pkgs = c("reshape2", "tidyverse", "plotly","upsetr","ggpubr","knitr", "CaTools", "bitops", "rprojroot", "rmarkdown","scales","gplots")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
 source('http://bioconductor.org/biocLite.R')
